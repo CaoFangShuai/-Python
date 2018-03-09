7d330111bcef6dd644f46a1b134afe83cd90b8fb
Jhone <15638113849@163.com>
2018-03-09 18:19:58 +0800

    2018-3-9 18:19:54

---
+++ 面向对象/class.py
@@ -0,0 +1,48 @@
+# -*-coding:utf-8-*-
+'''
+面向对象最重要的概念就是类（Class）和实例（Instance），必须牢记类是抽象的模板，比如Student类，而实例是根据类创建出来的一个个具体的“对象”，
+每个对象都拥有相同的方法，但各自的数据可能不同。
+仍以Student类为例，在Python中，定义类是通过class关键字：
+
+class后面紧接着是类名，即Student，类名通常是大写开头的单词，紧接着是(object)，表示该类是从哪个类继承下来的，
+继承的概念我们后面再讲，通常，如果没有合适的继承类，就使用object类，这是所有类最终都会继承的类。
+
+定义好了Student类，就可以根据Student类创建出Student的实例，创建实例是通过类名+()实现的：
+'''
+class Student(object):
+    pass
+bart = Student()
+print(bart)
+
+
+'''
+可以看到，变量bart指向的就是一个Student的实例，后面的0x10a67a590是内存地址，每个object的地址都不一样，而Student本身则是一个类。
+
+可以自由地给一个实例变量绑定属性，比如，给实例bart绑定一个name属性：
+'''
+bart.name = 'Bart Simpson'
+print bart.name
+# 由于类可以起到模板的作用，因此，可以在创建实例的时候，把一些我们认为必须绑定的属性强制填写进去。通过定义一个特殊的__init__方法，在创建实例的时候，就把name，score等属性绑上去：
+class Student1(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+# print(Student1.name)
+bart1=Student1("caofangshuai",26)
+print(bart1.name,bart1.score)
+'''
+注意：特殊方法“__init__”前后分别有两个下划线！！！
+注意到__init__方法的第一个参数永远是self，表示创建的实例本身，因此，在__init__方法内部，就可以把各种属性绑定到self，因为self就指向创建的实例本身。
+
+有了__init__方法，在创建实例的时候，就不能传入空的参数了，必须传入与__init__方法匹配的参数，但self不需要传，Python解释器自己会把实例变量传进去：
+'''
+# 数据封装
+# 面向对象编程的一个重要特点就是数据封装。在上面的Student类中，每个实例就拥有各自的name和score这些数据。我们可以通过函数来访问这些数据，比如打印一个学生的成绩
+def print_score(std):
+    return "%s:%s" %(std.name,std.score)
+print(print_score(bart1))
+'''
+但是，既然Student实例本身就拥有这些数据，要访问这些数据，就没有必要从外面的函数去访问，可以直接在Student类的内部定义访问数据的函数，这样，就把“数据”给封装起来了。
+
+这些封装数据的函数是和Student类本身是关联起来的，我们称之为类的方法：'''
